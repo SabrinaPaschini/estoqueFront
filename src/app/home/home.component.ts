2f5430bb35139938e93e3df7e3f0cea6d5b85cfc
Sabrina Paschini <sabrina.paschini@hotmail.com>
2025-04-14 11:08:13 -0300

    organizando objeto

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+interface Componente { // typescript 
+  tipo: string;
+  medida: string;
+  valor: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-
   componente = {
     tipo: '',
     medida: '',
     valor: '',
   };
 
+  componentes: Componente[] = []; // relacionado a interface, tipo componente 
 
-  adiconaComponente(){
+  constructor() {}
 
-    new this.componente = 
-    
+  ngOnInit(): void {}
+
+  chamaObjeto() {
+    console.log(this.componente);
+  }
+
+
+  salvaComponente() {
+    this.componentes.push({ ...this.componente }); // spread operator - cria uma copia do objeto atual 
+    this.componente = { tipo: '', medida: '', valor: '' };
   }
 }
